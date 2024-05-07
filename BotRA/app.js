42893e4df7cef8f88e82a886c1ca610e5b9076cf
sergio.merino <sergio.merino@alumnos.upm.es>
2024-05-07 16:27:41 +0200

    Quitar Influx Bot

--- BotRA/app.js
+++ BotRA/app.js
@@ -36,7 +36,15 @@ bot.on('message', async (msg) => {
     bot.sendMessage(chatId, 'Welcome to the bot!');
   }
   if (messageText === '/average') {
-    bot.sendMessage(chatId, obtenerDatosDelSensor());
+    try {
+      const response = await axios.get('http://10.100.0.102:5001/botaverage');
+      console.log('Datos del sensor:', response.data);
+      const msn = JSON.stringify(response.data);
+      bot.sendMessage(chatId, msn);
+    } catch (error) {
+      console.error('Error al obtener los datos del sensor:', error);
+  }
+
   }
 });
 
