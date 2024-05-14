990517c35c11ea6962f38589d35af05bce15d26d
sergio.merino <sergio.merino@alumnos.upm.es>
2024-05-14 16:34:31 +0200

    aaa

--- BotRA/app.js
+++ BotRA/app.js
@@ -57,7 +57,7 @@ async function obtenerDatosDelSensor(chatId) {
 async function obtenerDatosDelSensorUltimosSeg(chatId, segs){
 
   try {
-    await axios.sendMessage(urlAverage,"?timeinseconds="+segs);
+    await axios.send(urlAverage,"?timeinseconds="+segs);
     const response = await axios.get(urlAverage);
     console.log('Datos del sensor:', response.data);
     const msn = JSON.stringify(response.data);
