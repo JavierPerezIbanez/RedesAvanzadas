de64bc93686ed7d43a0bdd392a8a091c8e12a19e
Jorge Martin la Pena <jorge.martin.lapena@alumnos.upm.es>
2024-05-21 18:05:24 +0200

    Changes in time petitions

--- HTTP_MQTT_Middlewares/Petitions/app.js
+++ HTTP_MQTT_Middlewares/Petitions/app.js
@@ -47,7 +47,7 @@ function wait(ms) {
 }
 
 var numReq = 1000;
-var tEspera = 400;
+var tEspera = 300;
 // Función asincrónica para enviar las solicitudes de manera secuencial
 async function enviarSolicitudes() {
   for (let i = 0; i < numReq; i++) {
