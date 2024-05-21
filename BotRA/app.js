063e7c3d59eabafabd9812483df97e373d856358
sergio.merino <sergio.merino@alumnos.upm.es>
2024-05-21 15:43:59 +0200

    Last Value

--- BotRA/app.js
+++ BotRA/app.js
@@ -5,6 +5,8 @@ const axios = require('axios');
 const botToken = '6626788083:AAH37DXg5Zm9dGeodvvVXaha403Axm3xk30'; // Replace with your own bot token
 const bot = new TelegramBot(botToken, { polling: true });
 const urlAverage ="http://10.100.0.102:5001/botaverage";
+const urlLast ="http://10.100.0.102:5001/lastvalue";
+
 
 
 
@@ -35,7 +37,7 @@ bot.on('message', async (msg) => {
     obtenerDatosDelSensorUltimosSeg(chatId,segs);
   }else if (messageText === '/actual_'){
     const aux = messageText.split('_');
-    const variable= aux[1];
+    const variable= parseInt(aux[1]);
     obtenerUltimoDatoVariable(chatId,variable);
   }
 
@@ -66,16 +68,21 @@ async function obtenerDatosDelSensorUltimosSeg(chatId, segs){
 }
 }
 async function obtenerUltimoDatoVariable(chatId, variable){
-  const response = await axios.get(urlAverage);
-  console.log('Datos del sensor:', response.data.variable);
-  const msn = JSON.stringify(response.data.variable);
-  bot.sendMessage(chatId, msn);
+  
+  try {
+    const response = await axios.get(urlLast);
+    console.log('Datos del sensor:', response.data.variable);
+    const msn = JSON.stringify(response.data.variable);
+    bot.sendMessage(chatId, msn);
+} catch (error) {
+    console.error('Error al obtener los datos del sensor:', error);
+}
 }
 async function mostrarMenu(chatId){
   bot.sendMessage(chatId,"/help --> Mostrar comandos\n"+
                   "/average --> Mostrar media de datos de los ultimos 5 minutos\n"+
                   "/average_X --> X siendo el tiempo en segundos que quieres saber la media de todos los datos.\n"+
-                  "/actual_X -->X siendo una de las variables que son pedidas. Pueden ser: volatiles, co2, temperatura, humedad all para todo.\n"
+                  "/actual_X -->X siendo una de las variables que son pedidas. Pueden ser: volatiles, co2, temperatura, humedad o all para todo.\n"
    );
 }
 
